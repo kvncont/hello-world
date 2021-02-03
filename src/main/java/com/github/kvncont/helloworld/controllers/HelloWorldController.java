c8af0ae81072eec6d0b2bb99e2df636e80eb0e3b
Kevin Contreras <kvncont@gmail.com>
2021-02-02 22:40:24 -0600

    Fix 2

--- src/main/java/com/github/kvncont/helloworld/controllers/HelloWorldController.java
+++ src/main/java/com/github/kvncont/helloworld/controllers/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
     @RequestMapping("/")
 	public String index() {
-		System.out.println("Logs..");
+		System.out.println("Logs...");
 		return "Hello Wolrd from Spring Boot!";
 	}
     
