e5275a459e10049603a42be0485ed530331da494
Kevin Dario Contreras Rodriguez <kvncont@gmail.com>
2021-03-22 21:52:07 -0600

    Se agrega el chart de helm para mejorar el despliegue en k8s

--- src/main/java/com/github/kvncont/helloworld/controllers/HelloWorldController.java
+++ src/main/java/com/github/kvncont/helloworld/controllers/HelloWorldController.java
@@ -1,15 +1,18 @@
 package com.github.kvncont.helloworld.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+
+	@Value("${env.message}")
+	private String myEnvVar;
+
     @RequestMapping("/")
 	public String index() {
-		System.out.println("Branch... pr-10");
-		return "Hello Wolrd from Spring Boot! v3.1 pr-10";
+		return "Hello Wolrd from " + myEnvVar;
 	}
-    
 }
